67978549236d159712c0ca20d26f9251735dfacb
kustergabriel <gabrielazevedo1812@gmail.com>
2026-03-18 15:19:26 -0300

    Adicionado getVideo e getVideos

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -3,17 +3,43 @@ import videos from '../models/videosModels.js';
 class VideosController {
     // retorna a lista de videos
     static async getVideos (req,res) {
-        
+        try {
+            const resultado = await videos.retornaVideos();
+            res.status(200).json({
+                resultado
+            });
+        } catch (error) {
+            res.status(500).json({ erro: "Erro ao retornar vídeos" });
+        } 
     }
     // retorna apenas um video
     static async getVideo (req,res) {
+        try {
+            const id = req.params.id
+            const resultado = await videos.retornaVideo(id); // aqui ja retorna um objeto
+            if (resultado != 0) {
+                res.status (200).json({
+                    resultado
+                })
+            } else {
+                res.status(404).json ("Indice nao encontrado!")
+            }
+            
+        } catch (error) {
+            res.status(500).json({ erro: "Erro ao retornar vídeo" });
+        }
 
     }
     // cadastra um video
     static async postVideo (req,res) {
         try {
             const video = req.body;
-            res.status(201).json(video);
+            const resultado = await videos.adicionaVideo(video);
+            res.status(201).json({
+                mensagem: "Vídeo cadastrado com sucesso",
+                id: resultado.insertId,
+                video
+            });
         } catch (erro) {
             console.error(erro);
             res.status(500).json({ erro: "Erro ao cadastrar vídeo" });
--- src/models/videosModels.js
+++ src/models/videosModels.js
@@ -2,16 +2,48 @@ import database from '../config/dbconnect.js';
 
 
 class Video {
-    
+    // funcao para adicionar um video
     adicionaVideo(video) {
         const sql = 'INSERT INTO Videos SET ?';
 
-        database.query (sql, video, (erro, resultados) => {
-            if (erro) {
-                console.log(erro);
-            } else {
-                console.log(resultados);
-            }
+        return new Promise((resolve, reject) => {
+            database.query(sql, [video], (erro, resultados) => {
+                if (erro) {
+                    reject(erro);
+                } else {
+                    resolve(resultados);
+                }
+            })
+        })
+    }
+
+    // funcao para retornar todos os videos
+    retornaVideos () {
+        const sql = 'SELECT * FROM Videos';
+
+        return new Promise((resolve, reject) => {
+            database.query(sql, (erro, resultados) => {
+                if (erro) {
+                    reject(erro);
+                } else {
+                    resolve(resultados);
+                }
+            })
+        })
+    }
+
+    // funcao para retornar um video baseado no id 
+    retornaVideo (id) {
+        const sql = 'SELECT * FROM Videos WHERE id = ?'
+
+        return new Promise((resolve, reject) => {
+            database.query(sql, [id], (erro,resultados) => {
+                if (erro) {
+                    reject(erro)
+                } else {
+                    resolve(resultados)
+                }
+            })
         })
     }
 }
